8a0ff62e4fdcfab165fea91bf619764ab6e10f39
SometimesThinks <ycw2859@gmail.com>
2025-11-17 14:39:41 +0900

    browser: 텍스트 포맷팅하기 학습

--- web_browser.py
+++ web_browser.py
@@ -1,112 +1,209 @@
 import socket
 import ssl
 import tkinter
+import tkinter.font
+
 
 class URL:
-  def __init__(self, url):
-    self.scheme, url = url.split("://", 1)
-    assert self.scheme in ["http", "https"]
-    if "/" not in url:
-      url += "/"
-    self.host, url = url.split("/", 1)
-    self.path = "/" + url
-    # 기본 port 설정
-    if self.scheme == "http":
-      self.port = 80
-    elif self.scheme == "https":
-      self.port = 443
-    # 사용자가 port를 지정한 경우 포트 설정
-    if ":" in self.host:
-      self.host, self.port = self.host.split(":", 1)
-      self.port = int(self.port)
+    def __init__(self, url):
+        self.scheme, url = url.split("://", 1)
+        assert self.scheme in ["http", "https"]
+        if "/" not in url:
+            url += "/"
+        self.host, url = url.split("/", 1)
+        self.path = "/" + url
+        # 기본 port 설정
+        if self.scheme == "http":
+            self.port = 80
+        elif self.scheme == "https":
+            self.port = 443
+        # 사용자가 port를 지정한 경우 포트 설정
+        if ":" in self.host:
+            self.host, self.port = self.host.split(":", 1)
+            self.port = int(self.port)
 
-  def request(self):
-    s = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM, proto=socket.IPPROTO_TCP)
-    # scheme이 HTTPS일 경우 SSL 컨텍스트 생성
-    if (self.scheme == "https"):
-      ctx = ssl.create_default_context()
-      s = ctx.wrap_socket(s, server_hostname=self.host)
-    s.connect((self.host, self.port))
-    # request 생성 및 전송
-    request = "GET {} HTTP/1.0\r\n".format(self.path)
-    request += "Host: {}\r\n".format(self.host)
-    request += "\r\n"
-    s.send(request.encode("utf8"))
-    # response 읽기
-    response = s.makefile("r", encoding="utf8", newline="\r\n")
-    status = response.readline()
-    version, status, explanation = status.split(" ", 2)
-    response_headers = {}
-    # headers 읽기
-    while True:
-      line = response.readline()
-      if line == "\r\n": break
-      header, value = line.split(":", 1)
-      response_headers[header.casefold()] = value.strip()
-    assert "transfer-encoding" not in response_headers
-    assert "content-encoding" not in response_headers
-    body = response.read()
-    s.close()
-    return body
+    def request(self):
+        s = socket.socket(
+            family=socket.AF_INET, type=socket.SOCK_STREAM, proto=socket.IPPROTO_TCP
+        )
+        # scheme이 HTTPS일 경우 SSL 컨텍스트 생성
+        if self.scheme == "https":
+            ctx = ssl.create_default_context()
+            s = ctx.wrap_socket(s, server_hostname=self.host)
+        s.connect((self.host, self.port))
+        # request 생성 및 전송
+        request = "GET {} HTTP/1.0\r\n".format(self.path)
+        request += "Host: {}\r\n".format(self.host)
+        request += "\r\n"
+        s.send(request.encode("utf8"))
+        # response 읽기
+        response = s.makefile("r", encoding="utf8", newline="\r\n")
+        status = response.readline()
+        version, status, explanation = status.split(" ", 2)
+        response_headers = {}
+        # headers 읽기
+        while True:
+            line = response.readline()
+            if line == "\r\n":
+                break
+            header, value = line.split(":", 1)
+            response_headers[header.casefold()] = value.strip()
+        assert "transfer-encoding" not in response_headers
+        assert "content-encoding" not in response_headers
+        body = response.read()
+        s.close()
+        return body
+
+
+class Text:
+    def __init__(self, text):
+        self.text = text
+
+
+class Tag:
+    def __init__(self, tag):
+        self.tag = tag
+
 
 def lex(body):
-  text = ""
-  in_tag = False
-  for c in body:
-    if c == "<":
-      in_tag = True
-    elif c == ">":
-      in_tag = False
-    elif not in_tag:
-      text += c
-  return text
+    out = []
+    buffer = ""
+    in_tag = False
+    for c in body:
+        if c == "<":
+            if buffer:
+                out.append(Text(buffer))
+            buffer = ""
+            in_tag = True
+        elif c == ">":
+            in_tag = False
+            out.append(Tag(buffer))
+            buffer = ""
+        else:
+            buffer += c
+    if not in_tag and buffer:
+        out.append(Text(buffer))
+    return out
+
 
 WIDTH, HEIGHT = 800, 600
 HSTEP, VSTEP = 13, 18
 
 SCROLL_STEP = 100
 
-def layout(text):
-  display_list = []
-  cursor_x, cursor_y = HSTEP, VSTEP
-  for c in text:
-    display_list.append((cursor_x, cursor_y, c))
-    cursor_x += HSTEP
-    if cursor_x >= WIDTH - HSTEP:
-      cursor_y += VSTEP
-      cursor_x = HSTEP
-  return display_list
+FONTS = {}
+
+
+def get_font(size, weight, style):
+    key = (size, weight, style)
+    if key not in FONTS:
+        font = tkinter.font.Font(size=size, weight=weight, slant=style)
+        label = tkinter.Label(font=font)
+        FONTS[key] = (font, label)
+    return FONTS[key][0]
+
+
+class Layout:
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.display_list = []
+
+        self.cursor_x = HSTEP
+        self.cursor_y = VSTEP
+        self.weight = "normal"
+        self.style = "roman"
+        self.size = 12
+
+        self.line = []
+        for tok in tokens:
+            self.token(tok)
+        self.flush()
+
+    def token(self, tok):
+        # 단어 처리
+        if isinstance(tok, Text):
+            for word in tok.text.split():
+                self.word(word)
+        # 태그 처리
+        elif tok.tag == "i":
+            self.style = "italic"
+        elif tok.tag == "/i":
+            self.style = "roman"
+        elif tok.tag == "b":
+            self.weight = "bold"
+        elif tok.tag == "/b":
+            self.weight = "normal"
+        elif tok.tag == "small":
+            self.size -= 2
+        elif tok.tag == "/small":
+            self.size += 2
+        elif tok.tag == "big":
+            self.size += 4
+        elif tok.tag == "/big":
+            self.size -= 4
+        elif tok.tag == "br":
+            self.flush()
+        elif tok.tag == "/p":
+            self.flush()
+            self.cursor_y += VSTEP
+
+    def word(self, word):
+        font = get_font(self.size, self.weight, self.style)
+        w = font.measure(word)
+        if self.cursor_x + w >= WIDTH - HSTEP:
+            self.flush()
+        self.line.append((self.cursor_x, word, font))
+        self.cursor_x += w + font.measure(" ")
+
+    def flush(self):
+        if not self.line:
+            return
+        metrics = [font.metrics() for x, word, font in self.line]
+        max_ascent = max(metric["ascent"] for metric in metrics)
+        baseline = self.cursor_y + 1.25 * max_ascent
+        for x, word, font in self.line:
+            y = baseline - font.metrics("ascent")
+            self.display_list.append((x, y, word, font))
+        max_descent = max(metric["descent"] for metric in metrics)
+        self.cursor_y = baseline + 1.25 * max_descent
+        self.cursor_x = HSTEP
+        self.line = []
+
 
 class Browser:
-  def __init__(self):
-    self.window = tkinter.Tk()
-    self.canvas = tkinter.Canvas(self.window, width=WIDTH, height=HEIGHT)
-    self.canvas.pack()
-    self.scroll = 0
-    # 키 바인딩
-    self.window.bind("<Down>", self.scrolldown)
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.canvas = tkinter.Canvas(self.window, width=WIDTH, height=HEIGHT)
+        self.canvas.pack()
+        self.scroll = 0
+        self.window.bind("<Down>", self.scrolldown)
+        self.display_list = []
 
-  def load(self, url):
-    body = url.request()
-    text = lex(body)
-    self.display_list = layout(text)
-    self.draw()
-    
-  def draw(self):
-    self.canvas.delete("all")
-    for x, y, c in self.display_list:
-      if y > self.scroll + HEIGHT: continue
-      if y + VSTEP < self.scroll: continue
-      self.canvas.create_text(x, y - self.scroll, text=c)
-    
-  def scrolldown(self, e):
-    self.scroll += SCROLL_STEP
-    self.draw()
+    def load(self, url):
+        body = url.request()
+        tokens = lex(body)
+        self.display_list = Layout(tokens).display_list
+        self.draw()
 
-    
-    
+    def draw(self):
+        self.canvas.delete("all")
+        for x, y, word, font in self.display_list:
+            if y > self.scroll + HEIGHT:
+                continue
+            if y + font.metrics("linespace") < self.scroll:
+                continue
+            self.canvas.create_text(
+                x, y - self.scroll, text=word, font=font, anchor="nw"
+            )
+
+    def scrolldown(self, e):
+        self.scroll += SCROLL_STEP
+        self.draw()
+
+
 # 메인 함수
 if __name__ == "__main__":
-  import sys
-  Browser().load(URL(sys.argv[1]))
-  tkinter.mainloop()
+    import sys
+
+    Browser().load(URL(sys.argv[1]))
+    tkinter.mainloop()
